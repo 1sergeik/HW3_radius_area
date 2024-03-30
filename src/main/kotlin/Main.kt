728eb9ffe7f0afe8567f9c887c86e805219c1107
1sergeik <1sergeik@gmail.com>
2024-03-30 17:05:38 +0300

    HW3 start

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,20 @@
-package org.example
+import kotlin.math.*
 
 fun main() {
-    println("Hello World!")
+    println("Enter the radius of the circle:")
+    val radiusText_= readLine()
+    val radiusText = radiusText_.toString()
+    val radius: Float = radiusText.toFloat()
+    println("The circle area is:")
+    circleArea(radius)
+    println("The circle perimeter is:")
+    circlePerimeter(radius)
+}
+fun circleArea(radiusA:Float){
+    val circleAreaA= 3.4*(radiusA.pow(2))
+    println(circleAreaA)
+}
+fun circlePerimeter(radiusP:Float){
+    val circlePerimeterP= 2*3.14*radiusP
+    println(circlePerimeterP)
 }
